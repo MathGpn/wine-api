7c646eb21c0dd8927b7f23155a2762793fd095e4
MathGpn <grosperrin@eisti.eu>
2022-12-12 20:22:23 +0100

    Update for the best wine generation

--- app/scripts/predict_tools.py
+++ app/scripts/predict_tools.py
@@ -43,18 +43,57 @@ async def generate_best_wine(model : MODEL_TYPE)-> DataLine:
         DataLine: keys : fixed_acidity,volatile_acidity,residual_sugar,chlorides,free_sulfur_dioxide,total_sulfur_dioxide,density,pH,sulphates,alcohol type of values float
     """
     df = pd.read_csv(DATASET_FILE)
+    mean_fixed_acidity = 0
+    mean_volatile_acidity = 0
+    mean_alcohol = 0
+    mean_chlorides = 0 
+    mean_citric_acid = 0
+    mean_density = 0
+    mean_pH = 0
+    mean_residual_sugar = 0
+    mean_sulphates = 0
+    mean_free_sulfur_dioxide = 0
+    mean_total_sulfur_dioxide = 0
+    
     best_wines = df[df['quality'] > 7]
-    best_wine = DataLine(fixed_acidity = best_wines.iloc[0]['fixed acidity'],
-                         volatile_acidity = best_wines.iloc[0]['volatile acidity'],
-                         citric_acid = best_wines.iloc[0]['citric acid'],
-                         residual_sugar = best_wines.iloc[0]['residual sugar'],
-                         chlorides = best_wines.iloc[0]['chlorides'],
-                         free_sulfur_dioxide = best_wines.iloc[0]['free sulfur dioxide'],
-                         total_sulfur_dioxide = best_wines.iloc[0]['total sulfur dioxide'],
-                         density = best_wines.iloc[0]['density'],
-                         pH = best_wines.iloc[0]['pH'],
-                         sulphates = best_wines.iloc[0]['sulphates'],
-                         alcohol = best_wines.iloc[0]['alcohol'],
-                         quality = best_wines.iloc[0]['quality'] + 2
-                         )
+    for i in range (len(best_wines)) :
+        mean_fixed_acidity += best_wines.iloc[i][0] 
+        mean_volatile_acidity += best_wines.iloc[i][1]
+        mean_citric_acid += best_wines.iloc[i][2]
+        mean_residual_sugar += best_wines.iloc[i][3]
+        mean_chlorides += best_wines.iloc[i][4]
+        mean_free_sulfur_dioxide += best_wines.iloc[i][5]
+        mean_total_sulfur_dioxide += best_wines.iloc[i][6]
+        mean_density += best_wines.iloc[i][7]
+        mean_pH += best_wines.iloc[i][8]
+        mean_sulphates += best_wines.iloc[i][9]
+        mean_alcohol += best_wines.iloc[i][10]
+
+    mean_fixed_acidity = mean_fixed_acidity / len(best_wines)
+    mean_volatile_acidity = mean_volatile_acidity / len(best_wines)
+    mean_citric_acid = mean_citric_acid / len(best_wines)
+    mean_residual_sugar = mean_residual_sugar / len(best_wines)
+    mean_chlorides = mean_chlorides / len(best_wines)
+    mean_free_sulfur_dioxide = mean_free_sulfur_dioxide / len(best_wines)
+    mean_total_sulfur_dioxide = mean_total_sulfur_dioxide / len(best_wines)
+    mean_density = mean_density / len(best_wines)
+    mean_pH = mean_pH / len(best_wines)
+    mean_sulphates = mean_sulphates / len(best_wines)
+    mean_alcohol = mean_alcohol / len(best_wines)
+    mean_quality = 8
+
+    best_wine = DataLine(   
+                            fixed_acidity = mean_fixed_acidity,
+                            volatile_acidity = mean_volatile_acidity,
+                            citric_acid = mean_citric_acid,
+                            residual_sugar = mean_residual_sugar,
+                            chlorides = mean_chlorides,
+                            free_sulfur_dioxide = mean_free_sulfur_dioxide,
+                            total_sulfur_dioxide = mean_total_sulfur_dioxide,
+                            density = mean_density,
+                            pH = mean_pH,
+                            sulphates = mean_sulphates,
+                            alcohol = mean_alcohol,
+                            quality = mean_quality + 2
+                        )
     return best_wine
